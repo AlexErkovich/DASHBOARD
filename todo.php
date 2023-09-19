d2c128771a5d593ddac60a9bd3267fb897fdbd41
A45 | Alex Erkovich <a89005632281@yandex.ru>
2023-09-19 16:07:53 +0600

    Создал страницу с todo листом

--- todo.php
+++ todo.php
@@ -4,45 +4,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/reset.css">
+    <title>Document</title>
 </head>
 
 <body>
-    <header class="header">
-        </div>
-        <a href="index.html" class="header_logo-link">
-            <img src="img/logo.svg" alt="advancing" class="header_logo-pic">
-        </a>
-        <nav class="header_nav">
-            <ul class="header_list">
-                <li class="header_item">
-                    <a href="01.html" class="header_link">Связь и коммуникация</a>
-                </li>
-                <li class="header_item">
-                    <a href="02.html" class="header_link">Реклама</a>
-                </li>
-                <li class="header_item">
-                    <a href="03.html" class="header_link">Финансы</a>
-                </li>
-                <li class="header_item">
-                    <a href="04.html" class="header_link">Производство</a>
-                </li>
-                <li class="header_item">
-                    <a href="05.html" class="header_link">Качество</a>
-                </li>
-                <li class="header_item">
-                    <a href="06.html" class="header_link">PR</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
     <div class="todo">
 
-        <input type="text" id="connect" name="name" required minlength="4" maxlength="8" size="10" />
+        <div class="todo__block">
+            <label for="name">Communication and communication</label>
+            <div class="todo__content__row">
+                <input type="text" id="name" name="name" required minlength="60" maxlength="60" size="60">
+                <input id="row_01" type="checkbox">
+            </div>
+            <input id="btn" type="button" value="Добавить поле">
+        </div>
+        <div class="todo__block">
+            <label for="name">Advertisement</label>
+            <div class="todo__content__row">
+                <input type="text" id="name" name="name" required minlength="60" maxlength="60" size="60">
+                <input id="row_01" type="checkbox">
+            </div>
+            <input id="btn" type="button" value="Добавить поле">
+        </div>
+        <div class="todo__block">
+            <label for="name">Finance</label>
+            <div class="todo__content__row">
+                <input type="text" id="name" name="name" required minlength="60" maxlength="60" size="60">
+                <input id="row_01" type="checkbox">
+            </div>
+            <input id="btn" type="button" value="Добавить поле">
+        </div>
+        <div class="todo__block">
+            <label for="name">Production</label>
+            <div class="todo__content__row">
+                <input type="text" id="name" name="name" required minlength="60" maxlength="60" size="60">
+                <input id="row_01" type="checkbox">
+            </div>
+            <input id="btn" type="button" value="Добавить поле">
+        </div>
+        <div class="todo__block">
+            <label for="name">Quality</label>
+            <div class="todo__content__row">
+                <input type="text" id="name" name="name" required minlength="60" maxlength="60" size="60">
+                <input id="row_01" type="checkbox">
+            </div>
+            <input id="btn" type="button" value="Добавить поле">
+        </div>
+        <div class="todo__block">
+            <label for="name">PR</label>
+            <div class="todo__content__row">
+                <input type="text" id="name" name="name" required minlength="60" maxlength="60" size="60">
+                <input id="row_01" type="checkbox">
+            </div>
+            <input id="btn" type="button" value="Добавить поле">
+        </div>
+        <input id="btn-save" type="button" value="Выполнить">
     </div>
+    
 </body>
 
 </html>
